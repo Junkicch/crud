b9e1e634110b4c05771ed52f105d5c53a73a0505
Junkicch <76850261+Junkicch@users.noreply.github.com>
2025-02-21 09:38:37 -0300

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { 
+  getUsers, addUser, updateUser, deleteUser, 
+  getTODO, addTODO, updateTODO, deleteTODO, validaTODO, newDay, 
+  getData, 
+  getEmail,
+  forgotPassword,
+  resetPassword
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+// Rotas para usuários
+router.get("/users", getUsers);                
+router.post("/users", addUser);                
+router.put("/users/:id", updateUser);          
+router.delete("/users/:id", deleteUser);       
+
+// Rotas para tarefas TODO
+router.get("/todos/:idUsuarios", (req, res) => {
+  console.log("Requisição recebida para getTODO:", req.params.idUsuarios);
+  getTODO(req, res);
+});   
+router.post("/todos", addTODO);                
+router.put("/todos/:idToDo", updateTODO);      
+router.delete("/todos/:idToDo", deleteTODO);   
+router.put("/todos/validate/:idToDo", validaTODO); 
+
+
+router.get("/data", getData);                  
+router.put("/reset", newDay);     
+
+//rotas para recuperação de senha
+router.get("/email/:email", getEmail);
+router.put("/forgotpassword/:email", forgotPassword);
+router.put("/resetpassword/:token", resetPassword);
+export default router;
